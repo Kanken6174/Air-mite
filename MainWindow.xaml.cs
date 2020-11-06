3cd78e018c42eb58837401b1f65c1a9924f585fd
yogeoffre <yorick.geoffre@etu.uca.fr>
2020-11-06 18:34:13 +0100

    added draggable gui. TODO: fix drag offset

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,11 +15,15 @@ using System.Windows.Shapes;
 
 namespace AirMite
 {
+
     /// <summary>
     /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Point mouseClick;
+        private double canvasLeft;
+        private double canvasTop;
         public MainWindow()
         {
             InitializeComponent();
@@ -29,6 +33,25 @@ namespace AirMite
             grappeB4.Visibility = Visibility.Collapsed;
             grappeB5.Visibility = Visibility.Collapsed;
             grappeB6.Visibility = Visibility.Collapsed;
+
+            foreach (object obj in gappesB.Children)
+            {
+                try
+                {
+                    Image img = (Image)obj;
+                    img.PreviewMouseDown += new MouseButtonEventHandler(myimg_MouseDown);
+                    img.PreviewMouseMove += new MouseEventHandler(myimg_MouseMove);
+                    img.PreviewMouseUp += new MouseButtonEventHandler(myimg_MouseUp);
+                    img.TextInput += new TextCompositionEventHandler(myimg_TextInput);
+                    img.LostMouseCapture += new MouseEventHandler(myimg_LostMouseCapture);
+                    img.SetValue(Canvas.LeftProperty, 0.0);
+                    img.SetValue(Canvas.TopProperty, 0.0);
+                }
+                catch
+                {
+                    //do something
+                }
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -45,5 +68,43 @@ namespace AirMite
             grappeB5.Visibility = Visibility.Visible;
             grappeB6.Visibility = Visibility.Visible;
         }
+
+        void myimg_LostMouseCapture(object sender, MouseEventArgs e)
+{
+    ((Image)sender).ReleaseMouseCapture();
+}
+
+void myimg_TextInput(object sender, TextCompositionEventArgs e)
+{
+    ((Image)sender).ReleaseMouseCapture();
+}
+
+void myimg_MouseUp(object sender, MouseButtonEventArgs e)
+{
+    ((Image)sender).ReleaseMouseCapture();
+}
+
+void myimg_MouseMove(object sender, MouseEventArgs e)
+{
+    if (((Image)sender).IsMouseCaptured)
+    {
+        Point mouseCurrent = e.GetPosition(null);
+        double Left = mouseCurrent.X - canvasLeft;
+        double Top = mouseCurrent.Y - canvasTop;
+        ((Image)sender).SetValue(Canvas.LeftProperty, canvasLeft + Left);
+        ((Image)sender).SetValue(Canvas.TopProperty, canvasTop + Top);
+        canvasLeft = Canvas.GetLeft(((Image)sender));
+        canvasTop = Canvas.GetTop(((Image)sender));
     }
 }
+
+void myimg_MouseDown(object sender, MouseButtonEventArgs e)
+{
+    mouseClick = e.GetPosition(null);
+    canvasLeft = Canvas.GetLeft(((Image)sender));
+    canvasTop = Canvas.GetTop(((Image)sender));
+    ((Image)sender).CaptureMouse();
+}
+    }
+}
+
