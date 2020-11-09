8a01c39a0ac239d935c49abbc31487b549350037
yogeoffre <yorick.geoffre@etu.uca.fr>
2020-11-09 08:30:33 +0100

    still fixing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace AirMite
             {
                 try
                 {
-                    Image img = (Image)obj;
+                    Rectangle img = (Rectangle)obj;
                     Uri test = new Uri("0.png", UriKind.Relative);
  
                     img.PreviewMouseDown += new MouseButtonEventHandler(myimg_MouseDown);
@@ -70,9 +70,9 @@ namespace AirMite
                     Offset -= 110;
                     string name = "ditto" + counter;
 
-                    (this.FindName(name) as Image).Source = null;   // erases source
-                    (this.FindName(name) as Image).SetValue(Canvas.LeftProperty, -100.0 - Offset); // only accepts floats and doubles!
-                    (this.FindName(name) as Image).SetValue(Canvas.TopProperty, canvasTop);
+                    (this.FindName(name) as ImageBrush).ImageSource = null;   // erases ImageSource
+                    (this.FindName(name) as Rectangle).SetValue(Canvas.LeftProperty, -100.0 - Offset); // only accepts floats and doubles!
+                    (this.FindName(name) as Rectangle).SetValue(Canvas.TopProperty, canvasTop);
                 }
             
 
@@ -80,40 +80,40 @@ namespace AirMite
 
         void myimg_LostMouseCapture(object sender, MouseEventArgs e) 
         {
-            ((Image)sender).ReleaseMouseCapture();
+            ((Rectangle)sender).ReleaseMouseCapture();
         }
 
         void myimg_TextInput(object sender, TextCompositionEventArgs e)
         {
-            ((Image)sender).ReleaseMouseCapture();
+            ((Rectangle)sender).ReleaseMouseCapture();
         }
 
         void myimg_MouseUp(object sender, MouseButtonEventArgs e) // disables Hook on mouse position
         {
-            ((Image)sender).ReleaseMouseCapture();
+            ((Rectangle)sender).ReleaseMouseCapture();
             
         }
 
         void myimg_MouseMove(object sender, MouseEventArgs e) // changes variables accordingly if mouse moves
         {
-            if (((Image)sender).IsMouseCaptured)
+            if (((Rectangle)sender).IsMouseCaptured)
             {
                 Point mouseCurrent = e.GetPosition(null);
                 double Left = mouseCurrent.X - canvasLeft - 830; // yes i know substracting the card center afterwards is ugly,
                 double Top = mouseCurrent.Y - canvasTop - 80;  // but it'll work for now
-                ((Image)sender).SetValue(Canvas.LeftProperty, canvasLeft + Left); // Sets new position on canvas for clicked image
-                ((Image)sender).SetValue(Canvas.TopProperty, canvasTop + Top); //
-                canvasLeft = Canvas.GetLeft(((Image)sender));
-                canvasTop = Canvas.GetTop(((Image)sender));
+                ((Rectangle)sender).SetValue(Canvas.LeftProperty, canvasLeft + Left); // Sets new position on canvas for clicked image
+                ((Rectangle)sender).SetValue(Canvas.TopProperty, canvasTop + Top); //
+                canvasLeft = Canvas.GetLeft(((Rectangle)sender));
+                canvasTop = Canvas.GetTop(((Rectangle)sender));
             }
         }
 
         void myimg_MouseDown(object sender, MouseButtonEventArgs e) // checks for mouse click on children of canvas (drag)
         {
             mouseClick = e.GetPosition(null);
-            canvasLeft = Canvas.GetLeft(((Image)sender));   // get left coordinates of clicked picture
-            canvasTop = Canvas.GetTop(((Image)sender));     // get top coordinates of clicked picture
-            ((Image)sender).CaptureMouse();
+            canvasLeft = Canvas.GetLeft(((Rectangle)sender));   // get left coordinates of clicked picture
+            canvasTop = Canvas.GetTop(((Rectangle)sender));     // get top coordinates of clicked picture
+            ((Rectangle)sender).CaptureMouse();
         }
 
         private void NvTirage(object sender, RoutedEventArgs e)
@@ -123,8 +123,8 @@ namespace AirMite
             int counter = 0;
             int Offset = 00;
             Random rnd = new Random();
-            while (counter < 3)
-            {
+            /*while (counter < 3)
+            {*/
                 counter++;
                 Offset -= 110;
                 string name = "ditto" + counter;
@@ -134,22 +134,38 @@ namespace AirMite
                     clonecount = 0;
                 }
 
-                Uri Carte = new Uri(clonecount + ".png", UriKind.Relative);     //CCreates Uri element, (sort of fileHandle for c#)
-                (this.FindName(name) as Image).Source = new BitmapImage(Carte);
-                (this.FindName(name) as Image).SetValue(Canvas.LeftProperty, -100.0 - Offset); // only accepts floats and doubles!
-                (this.FindName(name) as Image).SetValue(Canvas.TopProperty, canvasTop);
-            }
-            if (ditto2.Source == ditto3.Source)
+                /*Uri Carte = new Uri(clonecount + ".png", UriKind.Relative);     //CCreates Uri element, (sort of fileHandle for c#)*/
+
+
+                /*ImageBrush ib = new ImageBrush();
+                ib.ImageSource = new ImageBrush(new BitmapImage(Carte)); // NEEDS FIXING*/
+                Rectangle ditto4 = new Rectangle();
+                Canvas.SetLeft(ditto4, 150);
+                Canvas.SetTop(ditto4, 130);
+                ditto4.StrokeThickness = 10;
+                ditto4.Height = 200;
+                ditto4.Width = 140;
+                ditto4.SetValue(Canvas.LeftProperty, 100d);
+                ditto4.SetValue(Canvas.TopProperty, 100d);
+                Uri Carte = new Uri("pack://AirMite:,,,/Resources/"+ clonecount + ".png", UriKind.Relative);
+                BitmapImage CardFace = new BitmapImage(Carte);
+                ditto4.Fill = new ImageBrush(CardFace);
+                Tripletirage.Children.Add(ditto4);  //is a valid canvas
+                /*(this.FindName(name) as Rectangle).SetValue(Canvas.LeftProperty, -100.0 - Offset); // only accepts floats and doubles!
+                (this.FindName(name) as Rectangle).SetValue(Canvas.TopProperty, canvasTop);*/
+            //}
+            /*if (ditto2.ImageSource == ditto3.ImageSource)
             {
-                clause.Content = ditto1.Source;
+                clause.Content = ditto1.ImageSource;
             }
             else
             {
                 clause.Content = "nope";
             }
-            c2.Content = "source de Ditto1 : " + ditto1.Source;
-            c3.Content = "source de Ditto2 : " + ditto2.Source;
-            c4.Content = "source de Ditto3 : " + ditto3.Source;
+            c2.Content = "ImageSource de Ditto1 : " + ditto1.ImageSource;
+            c3.Content = "ImageSource de Ditto2 : " + ditto2.ImageSource;
+            c4.Content = "ImageSource de Ditto3 : " + ditto3.ImageSource;
+            */
         }
 
     }
